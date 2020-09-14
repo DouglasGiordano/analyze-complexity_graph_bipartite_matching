780ecbf3a0bcf5545a1eea01bafbb7434c1ac9f6
DouglasGiordano <douglasmontanhagiordano@gmail.com>
2020-09-14 16:34:49 -0300

    Fixed curve fit

--- graph/create_fit_curve.py
+++ graph/create_fit_curve.py
@@ -1,39 +1,32 @@
-import warnings
-
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 from scipy import optimize
-from scipy.optimize import differential_evolution
+from scipy.optimize import curve_fit
+
 df = pd.read_csv("output/result_matching.csv")
 
-y_data = np.array((df['time']))
-x_data = np.array((df['edges']))
+y_data = np.array((df['edges']))
+x_data = np.array((df['time']))
 
 # And plot it
 
 plt.figure(figsize=(6, 4))
 plt.scatter(x_data, y_data)
 
-############################################################
-# Now fit a simple sine function to the data
-def test_func(x, a, b):
-    print("a " +str(a))
-    print("b " +str(b))
-    print("x " + str(x))
-    return (x**(a/b))
+def func(x, a, b):
+    #return a * np.exp(b * x)
+    return a*x + b
 
-params, params_covariance = optimize.curve_fit(test_func, x_data, y_data,
-                                               p0=[1,1])
+params = curve_fit(func, x_data, y_data)
 
-print(params)
+[a, b] = params[0]
 
-############################################################
 # And plot the resulting curve on the data
-
+plt.scatter(x_data, y_data, label='Data')
 plt.figure(figsize=(6, 4))
 plt.scatter(x_data, y_data, label='Data')
-plt.plot(x_data, test_func(x_data, params[0], params[1]),label='Fitted function')
+plt.plot(x_data, func(x_data, a, b),label='Fitted function')
 
 plt.legend(loc='best')
 
